6838d156839c23957a3d753ded49326034feea32
ChenZheng <boch2436@gmail.com>
2015-11-01 18:32:41 +0800

    Initial Commit

---
+++ BluetoothCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BluetoothCamera
+//
+//  Created by ChenZheng on 15/11/1.
+//  Copyright © 2015年 QiuShiBaiKe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
